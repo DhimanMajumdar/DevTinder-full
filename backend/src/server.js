26baf058249567117e82a873af4e4f26c6fbaa6c
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-06-16 18:59:38 +0530

    routing basic

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,15 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Dhiman", lastName: "Majumdar" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Saving data to db");
+});
 app.use("/test", (req, res) => {
   res.send("test from the server");
-});
-app.use("/hello", (req, res) => {
-  res.send("Hello from the server");
 });
 
 app.listen(7777, () => {
